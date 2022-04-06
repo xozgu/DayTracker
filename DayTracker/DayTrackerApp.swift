2f543bb7d25d8fa7783c588224c10a72f11bc16e
Ozgu Ozden <ozgu@theshell.co.jp>
2022-04-06 16:14:43 +0900

    Initial Commit

---
+++ DayTracker/DayTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayTrackerApp.swift
+//  DayTracker
+//
+//  Created by Ozgu Ozden on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct DayTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
